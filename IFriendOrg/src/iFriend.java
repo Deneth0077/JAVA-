2d57b33168c29cabfc4dae13b55b448fb63c140f
CHATHURANGA KHD <144873531+Deneth0077@users.noreply.github.com>
2024-12-01 22:47:56 +0530

    Add files via upload

---
+++ IFriendOrg/src/iFriend.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class iFriend {
+
+    private JPanel panel1;
+    private JPanel panel2;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
